98a0cafee81be007cebcf8174d29f84660b444bf
alejandromtz04 <not4lmas@gmail.com>
2024-12-16 11:48:28 -0600

    DTO's

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,37 @@
-export class CreateUserDto {}
+import { IsEmail, IsNumber, IsString, IsBoolean, MinLength, IsNotEmpty, IsOptional, IsInt } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    age: number;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @MinLength(6)
+    @IsNotEmpty()
+    password: string;
+    
+    @IsInt()
+    @IsNotEmpty()
+    userContact: number;
+
+    @IsInt()
+    @IsNotEmpty()
+    userProfile: number;
+
+    @IsBoolean()
+    @IsOptional()
+    state: boolean;
+}
